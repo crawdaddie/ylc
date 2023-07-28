885eb1eee9631bffe879531ea4614366aac90423
Adam Juraszek <adam@Adams-MacBook-Pro.local>
2023-07-28 08:52:19 +0100

    .

--- tests/parse_test.c
+++ tests/parse_test.c
@@ -34,6 +34,7 @@ static void test_parse(char *input, int length, ...) {
   assert_ast_compare(
       ast, AST_NEW(MAIN, expected_statements), // wrap statements in 'main' ast
       input);
+  free_ast(ast);
 }
 
 int main() {
@@ -207,7 +208,7 @@ int main() {
 
   /*
    * Type declarations
-   * */
+   */
 
   /* aliasing types */
   test_parse("type NewType = int8", 1,
@@ -238,5 +239,34 @@ int main() {
   test_parse("type FnType = fn (int a, int b) int", 1,
              AST_NEW(TYPE_DECLARATION, "FnType",
                      AST_NEW(FN_PROTOTYPE, 2, fn_proto_params, "int")));
+
+  AST *fn_proto_params2[] = {
+      AST_NEW(SYMBOL_DECLARATION, "x", "int"),
+      AST_NEW(SYMBOL_DECLARATION, "y", "int"),
+  };
+  test_parse("let h = fn (int x, int y) int {x + y}", 1,
+             AST_NEW(FN_DECLARATION,
+                     AST_NEW(FN_PROTOTYPE, 2, fn_proto_params2, "int"),
+                     AST_NEW(BINOP, TOKEN_PLUS, AST_NEW(IDENTIFIER, "x"),
+                             AST_NEW(IDENTIFIER, "y")),
+                     "h"));
+/*
+  AST *fn_proto_params3[] = {
+      AST_NEW(SYMBOL_DECLARATION, "x_", "int"),
+      AST_NEW(SYMBOL_DECLARATION, "y_", "int"),
+  };
+  test_parse(
+      "let h = fn (int x_, int y_) int\n"
+    "{let j = 1\nx_ + y_ + j}", 1,
+      AST_NEW(FN_DECLARATION, AST_NEW(FN_PROTOTYPE, 2, fn_proto_params3, "int"),
+              ast_statement_list(
+                  2, AST_NEW(ASSIGNMENT, "j", NULL, AST_NEW(INTEGER, 1)),
+                  AST_NEW(BINOP, TOKEN_PLUS,
+                          AST_NEW(BINOP, TOKEN_PLUS,
+                                  AST_NEW(IDENTIFIER, "x_"),
+                                  AST_NEW(IDENTIFIER, "y_")),
+                          AST_NEW(IDENTIFIER, "j"))),
+              "h"));
+  */
   return test_result;
 }
