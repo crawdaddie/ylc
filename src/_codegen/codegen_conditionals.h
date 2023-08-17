f27d22baeffa0658a9f68cad6eaaf1f9897228f5
Adam Juraszek <adam@Adams-MacBook-Pro.local>
2023-08-17 23:05:48 +0100

    cleanup

--- src/_codegen/codegen_conditionals.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LANG_CODEGEN_CONDITIONALS_H
-#define _LANG_CODEGEN_CONDITIONALS_H
-#include "../ast.h"
-#include "../llvm_backend.h"
-#include <llvm-c/Core.h>
-
-LLVMValueRef codegen_if_else(AST *ast, Context *ctx);
-LLVMValueRef codegen_match(AST *ast, Context *ctx);
-#endif /* ifndef _LANG_CODEGEN_CONTROL_FLOW_H */
