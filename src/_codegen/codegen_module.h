395b93926b3652a208ad93bfaaeb987601d41880
Adam Juraszek <adam@Adams-MacBook-Pro.local>
2023-08-18 09:40:36 +0100

    rm old codegen

--- src/_codegen/codegen_module.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LANG_CODEGEN_MODULE_H
-#define _LANG_CODEGEN_MODULE_H
-#include "../llvm_backend.h"
-#include <llvm-c/Core.h>
-
-LLVMValueRef codegen_module(char *filename, Context *ctx);
-#endif /* ifndef _LANG_CODEGEN_MODULE_H */
